a97564cad9553da254a79d69177263c8179f316c
gorkembayraktar <myemail>
2024-03-20 17:25:23 +0300

    copyright

--- src/views/layout/ListLeftMenu.jsx
+++ src/views/layout/ListLeftMenu.jsx
@@ -1,17 +1,13 @@
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import GraphicEqIcon from '@mui/icons-material/GraphicEq';
-import DashboardIcon from '@mui/icons-material/Dashboard';
 import TranslateIcon from '@mui/icons-material/Translate';
 import VideoSettingsIcon from '@mui/icons-material/VideoSettings';
 import FontDownloadIcon from '@mui/icons-material/FontDownload';
 import ListIcon from '@mui/icons-material/List';
-import BurstModeIcon from '@mui/icons-material/BurstMode';
-import { Divider, ListSubheader } from '@mui/material';
+import { Divider, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import AudiotrackIcon from '@mui/icons-material/Audiotrack';
@@ -51,21 +47,31 @@ export const ListLeftMenu = () => {
     const location = useLocation();
 
     return <List>
-               
-                {ROUTES.map((ROUTE, index) => (
-                <ListItem key={index} disablePadding component={Link} to={ROUTE.to} >
-                    <ListItemButton selected={location.pathname == ROUTE.to}>
+
+        {ROUTES.map((ROUTE, index) => (
+            <ListItem key={index} disablePadding component={Link} to={ROUTE.to} >
+                <ListItemButton selected={location.pathname == ROUTE.to}>
                     <ListItemIcon>
-                        { ROUTE.icon }
+                        {ROUTE.icon}
                     </ListItemIcon>
-                    <ListItemText  secondary={ROUTE.text} />
-                    </ListItemButton>
-                </ListItem>
-                ))}
+                    <ListItemText secondary={ROUTE.text} />
+                </ListItemButton>
+            </ListItem>
+        ))}
 
-                <Divider />
+        <Divider />
+        <Typography sx={{ mt: 2 }} variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
 
-            </List>
+            <Link to="https://smurftheme.net/" style={{ color: 'info', textDecoration: 'inherit' }} target="_blank" rel="noopener noreferrer" >
+                Smurf Theme
+            </Link>
+            {' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+
+    </List>
 }
 
 export default ListLeftMenu
--- src/views/pages/Profile.jsx
+++ src/views/pages/Profile.jsx
@@ -1,32 +1,21 @@
 
-import { Alert, Box, Button, ButtonGroup, Chip, Collapse, Divider, Grid, IconButton, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Paper, TextField, Tooltip, Typography } from '@mui/material';
-import Container from '@mui/material/Container';
-import EventIcon from '@mui/icons-material/Event';
-import AddIcon from '@mui/icons-material/Add';
-
-import {randevu_saatleri, DAYS, DAY_KEYS} from '../../data/constant'
-
-import {ByGroupByData} from '../../utils/array'
-
-import CreateWeeklyHour from '../components/modal/CreateWeeklyHour'
-
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-
-import {useTitle} from '../../hooks/useTitle'
+import { Grid, Paper } from '@mui/material';
+import { useTitle } from '../../hooks/useTitle'
 
 
-const Profile = () =>{
+const Profile = () => {
 
   useTitle("Profil");
 
   const mode = 'dark';
   return (
-      <>
-        <Grid container spacing={2}>
-            Profil sayfası..
-        </Grid>
-      </>
+    <Paper elevation={2}>
+      <Grid container spacing={2}>
+
+        adsad
+
+      </Grid>
+    </Paper>
   );
 }
 
